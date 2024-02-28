5cd18e31c06fdc3b7cce58ce845b1bbf6d4654ce
maskimmm <rifqi231@gmail.com>
2024-02-28 10:08:55 +0700

    refactor: now support start from ios 15

---
+++ Loan Management/Helpers/SortType.swift
@@ -0,0 +1,39 @@
+//
+//  SortType.swift
+//  Loan Management
+//
+//  Created by Rifqi Alhakim Hariyantoputera on 28/02/24.
+//
+
+import Foundation
+enum SortType: String, CaseIterable {
+    case nameAsc
+    case nameDesc
+    case amountLowest
+    case amountHighest
+    case purposeAsc
+    case purposeDesc
+    case termLowest
+    case termHighest
+    
+    var description: String {
+        switch self {
+        case .nameAsc:
+            return "Name Ascending"
+        case .nameDesc:
+            return "Name Descending"
+        case .amountLowest:
+            return "Lowest Amount"
+        case .amountHighest:
+            return "Highest Amount"
+        case .termLowest:
+            return "Lowest Term"
+        case .termHighest:
+            return "Highest Term"
+        case .purposeAsc:
+            return "Purpose Ascending"
+        case .purposeDesc:
+            return "Purpose Descending"
+        }
+    }
+}
